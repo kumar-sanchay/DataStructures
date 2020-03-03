a0cb255fd9339dd4fcfe36c0c78f24b789322d7d
Sanchay Kumar <51812506+kumar-sanchay@users.noreply.github.com>
2020-03-03 16:56:25 +0530

    mergesort

---
+++ mergesort.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void random_array(int *, int);
+void merge(int *, int, int, int);
+void mergesort(int*, int, int);
+void print_array(int *, int);
+
+void main(){
+	
+	int n;
+	printf("Enter the size of the array: ");
+	scanf("%d", &n);
+	int array[n];
+	random_array(array, n);
+	printf("Initial array: ");
+	print_array(array, n);
+	mergesort(array, 0, n);
+	printf("Sorted array: ");
+	print_array(array, n);
+	
+
+}
+
+void random_array(int *arr, int size){
+	int i;
+	for(i=0; i<size; i++){
+		arr[i]=(rand()%(99-10))+10;
+	}
+}
+
+void merge(int *arr, int l, int m, int r){
+	
+	int n1 = m - l + 1;
+	int n2 = r - m;
+	int L[n1], R[n2];
+	int i, j, k;
+	for(i=0; i<n1;i++){
+		L[i]=arr[l+i];
+	}
+	for(j=0; j<n2; j++){
+		R[j]=arr[m+j+1];
+	}
+	
+	i=0;
+	j=0;
+	k=l;
+	while(i<n1 && j<n2){
+		if(L[i]<=R[j]){
+			arr[k]=L[i];
+			i++;
+		}else{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	
+	while(i<n1){
+		arr[k]=L[i];
+		i++;
+		k++;
+	}
+	
+	while(j<n2){
+		arr[k]=R[j];
+		j++;
+		k++;
+	}
+
+}
+
+void mergesort(int *arr, int l, int r){
+
+	if(l<r){
+		int m = (l+r)/2;
+		mergesort(arr, l, m);
+		mergesort(arr, m+1, r);
+		merge(arr, l, m, r);
+	}
+}
+
+void print_array(int *arr, int size){
+	int i;
+	for(i=0; i<size; i++){
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+};
+
